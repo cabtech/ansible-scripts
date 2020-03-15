8beff7ca6dd32b4fd80e89159a814bdc3c50cb89
Tim Short <tim.short@cabernet.tech>
2020-03-15 20:05:07 +0000

    repo reorg

--- post-install-ansible-scripts.sh
+++ post-install-ansible-scripts.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 base=$(cd $(dirname $0) && pwd)
+
 cp $base/ct-*.sh /usr/local/bin
+chmod 755 /usr/local/etc/bash.d/ct-*.sh
+
 cp $base/ansible.inc /usr/local/etc/bash.d/ansible.sh
 chmod 644 /usr/local/etc/bash.d/ansible.sh
+
 exit 0
